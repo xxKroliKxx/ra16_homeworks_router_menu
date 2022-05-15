6403dbeb4f2171c597b16784f1b5412ebdcb81fb
rka <russkikh_k@ekb.sima-land.ru>
2022-05-15 21:40:15 +0500

    +

---
+++ README.md
@@ -0,0 +1,1 @@
+# ra16_homeworks_router_menu
---
+++ src/App.js
@@ -0,0 +1,103 @@
+import './App.css';
+import {BrowserRouter, Routes, Route, NavLink} from "react-router-dom"
+
+function Menu() {
+    const isActive = 'menu__item menu__item-active';
+    const isNoActive = 'menu__item';
+    return (
+        <nav className="menu">
+            <NavLink to="/" className={(navData) => navData.isActive ? isActive : isNoActive}>Главная</NavLink>
+            <NavLink to="/drift" className={(navData) => navData.isActive ? isActive : isNoActive}>Дрифт-такси</NavLink>
+            <NavLink to="/timeattack" className={(navData) => navData.isActive ? isActive : isNoActive}>Time Attack</NavLink>
+            <NavLink to="/forza" className={(navData) => navData.isActive ? isActive : isNoActive}>Forza Karting</NavLink>
+        </nav>
+    )
+}
+
+function HomePage() {
+    return (
+        <article className="article">
+            <h1 className="article__title">Гоночн​ое такси</h1>
+            <p className="article__paragraph">
+                Гоночн​ое такси – отличная возможность насладиться скоростью и мастерством гонщика,
+                сидя на месте штурмана, и стать свидетелем настоящего мастерства профессиональных
+                инструкторов Сочи Автодрома, в полной мере ощутив крутые виражи на самой современной
+                гоночной трассе России.
+            </p>
+        </article>
+    )
+}
+
+function DriftPage() {
+    return (
+        <article className="article">
+            <h1 className="article__title">Дрифт-такси</h1>
+            <p className="article__paragraph">
+                Только ​на Сочи Автодроме вас ждет уникальная возможность
+                промчаться по трассе Формулы 1 на максимально возможной
+                скорости в управляемом заносе на легендарной «королеве дрифта» Nissan Silvia!
+            </p>
+            <p className="article__paragraph">
+                Прокатитесь на дрифт-такси с вице-чемпионом Межконтинентального
+                кубка по дрифту под эгидой FIA 2017 года Аркадием Цареградцевым и
+                испытайте на себе, что значат скоростные постановки и эффектные заносы!
+            </p>
+        </article>
+    )
+}
+
+function ForzaPage() {
+    return (
+        <article className="article">
+            <h1 className="article__title">Forza Karting Sochi</h1>
+            <p className="article__paragraph">
+                В Олимпийском парке работает новый открытый развлекательный картинг центр мирового уровня!
+            </p>
+            <p className="article__paragraph">
+                Вас ждут большая, широкая, безопасная трасса и супер современные карты
+                европейского производства c двигателями мощностью 13 л.с.
+                Трасса длинной более 500 метров оборудована системой хронометража,
+                фиксирующей данные заезда в режиме реального времени.
+                По окончании заезда каждый пилот получает распечатку со своими результатами.
+            </p>
+        </article>
+    )
+}
+
+function TimeAttackPage() {
+    return (
+        <article className="article">
+            <h1 className="article__title">Гонка ​​​​​​Time Attack</h1>
+            <p className="article__paragraph">
+                Гонка Time Attack создана для тех, кто любит ставить рекорды –
+                открытое соревнование на гражданских автомобилях не предполагает
+                контактной борьбы пилотов во время заезда, но позволит в полной мере
+                ощутить скорость, найти идеальную траекторию и реализовать весь
+                потенциал своего автомобиля. Каждый участник проезжает по Сочи Автодрому
+                определённое количество кругов, а в зачёт идёт только лучшее время одного
+                круга из всех заездов. Для того, чтобы попробовать свои силы на лучшей
+                гоночной трассе страны, Вам понадобится исправный легковой автомобиль
+                на летних шинах и шлем.
+            </p>
+        </article>
+    )
+}
+
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <Menu/>
+                <div className="page">
+                    <Routes>
+                        <Route path="/drift" element={<DriftPage/>}/>
+                        <Route path="/timeattack" element={<TimeAttackPage/>}/>
+                        <Route path="/forza" element={<ForzaPage/>}/>
+                        <Route path="*" element={<HomePage/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
+}
